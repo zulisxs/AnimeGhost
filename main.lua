9053abbb46864d5ba408b1caf45618402c68e20f
zulisxs <zulisxs021380@gmail.com>
2026-02-21 15:55:51 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zulisxs/AnimeGhost/refs/heads/main/Utils/SearhEnemies.lua"))()
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/zulisxs/AnimeGhost/refs/heads/main/ui.lua"))()
