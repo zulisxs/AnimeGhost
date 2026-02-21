ee0f18bee5eef6c59c3532bedb95f94781e3197d
zulisxs <zulisxs021380@gmail.com>
2026-02-21 16:03:21 -0300

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -1,5 +1,5 @@
 local Fluent, SaveManager, InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/Beta.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/zulisxs/AnimeGhost/refs/heads/main/Utils/SearhEnemies.lua"))()
+local searchEnemies = loadstring(game:HttpGet("https://raw.githubusercontent.com/zulisxs/AnimeGhost/refs/heads/main/Utils/SearhEnemies.lua"))()
 
 local Window = Fluent:CreateWindow({
     Title = "zUlisxs Hub",
@@ -39,9 +39,7 @@ local selectenemiesDropdown = AutoFarmSection:AddDropdown("selectenemiesDropdown
 AutoFarmSection:AddButton({
     Title = "Reset enemies", Icon = "sparkles",
     Callback = function()
-        local enemies = searchEnemies()
-        selectenemiesDropdown:SetValues(enemies)
-        print("Enemies refreshed:", #enemies, "found")
+        selectenemiesDropdown:SetValues(searchEnemies())
     end
 })
 
